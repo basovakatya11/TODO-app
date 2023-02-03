7024fee4c5d366905c74586e174895c2e899fad3
basovakatya11 <106011130+basovakatya11@users.noreply.github.com>
2023-02-03 16:03:59 +0100

    Add files via upload

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import TasksFilter from '../TasksFilter'
+
+function Footer({ todo, onClearCompleted, onTasksFilter }) {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{todo} items left</span>
+      <TasksFilter onTasksFilter={onTasksFilter} />
+      <button type="button" className="clear-completed" onClick={onClearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  )
+}
+
+Footer.propTypes = {
+  todo: PropTypes.number.isRequired,
+  onClearCompleted: PropTypes.func.isRequired,
+  onTasksFilter: PropTypes.func.isRequired,
+}
+
+export default Footer
